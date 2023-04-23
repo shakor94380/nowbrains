79c71b4a78ebf52c2811f68c272d5ecd1982849d
shakor94380 <shakor94380@gmail.com>
2023-04-24 00:11:10 +0200

    add angular universal for SEO

--- front-angular/src/app/app.module.ts
+++ front-angular/src/app/app.module.ts
@@ -16,7 +16,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     AppComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule,
     SharedModule,
     ProductModule,
