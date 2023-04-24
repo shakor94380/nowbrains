f9cb42fbbe7bca53a4d0cbb96498dd06e62bdd09
agbo anthony <agboanthony@MacBook-Pro-de-agbo.local>
2023-04-24 13:53:49 +0200

    commit api final

--- product-api/src/configs/environement.config.ts
+++ product-api/src/configs/environement.config.ts
@@ -4,11 +4,14 @@ dotenv.config();
 
 const environements: any = {
     port: process.env.PORT,
-    mongoUrl: process.env.MONGO_URL,
+    mongoUrl: process.env.MONGO_SRV,
     mongoUser: process.env.MONGO_USER,
     mongoPwd: process.env.MONGO_PASSWORD,
     mongoDb: process.env.MONGO_DATABASE,
-    secret: process.env.SECRET
+    secret: process.env.SECRET,
+    isMongoSrv: process.env.IS_SRV,
+    mongoPort: process.env.MONGO_PORT,
+    mongoHost: process.env.MONGO_HOST
 };
 
 export default environements;
--- product-api/src/loaders/mongo.ts
+++ product-api/src/loaders/mongo.ts
@@ -1,7 +1,23 @@
 import mongoose from 'mongoose';
 import env from '../configs/environement.config';
 
-const uri: string = `mongodb+srv://${env.mongoUser}:${env.mongoPwd}@${env.mongoUrl}.mongodb.net/${env.mongoDb}?retryWrites=true&w=majority&useNewUrlParser=true&useUnifiedTopology=true`;
+let uri: string;
+let isSrv = env.isMongoSrv;
+let credential: string;
+
+let pwd = env.mongoPwd;
+let user = env.mongoUser;
+
+credential = user.length > 0 ? user : '';
+if (pwd.length > 0 && credential.length > 0) {
+    credential += `:${pwd}@`;
+}
+
+if (isSrv == 1) {
+    uri = `mongodb+srv://${credential}${env.mongoUrl}/${env.mongoDb}?retryWrites=true&w=majority&useNewUrlParser=true&useUnifiedTopology=true`;
+} else {
+    uri = `mongodb://${credential}${env.mongoHost}:${env.mongoPort}/${env.mongoDb}`;
+}
 
 mongoose.connect(uri)
 .then(() => console.log('connected to database'))
